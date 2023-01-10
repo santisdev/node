a4887dff4c571dc200145fa1b92d0814ce643571
santisdev <104403779+santisdev@users.noreply.github.com>
2023-01-10 14:10:04 -0300

    Adding routes file

--- 3/server/server.js
+++ 3/server/server.js
@@ -1,6 +1,6 @@
 const http = require("http");
-const fs = require("fs");
 
+const routes = require("./routes");
 // function rqListener(req, res) {}
 
 // http.createServer(rqListener);
@@ -12,29 +12,6 @@ const fs = require("fs");
 // // is the same as
 // http.createServer((req, res) => {});
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    res.write("<html>Home</html>");
-    return res.end();
-  }
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-    req.on("end", () => {
-      //To be run in the future, does not pause the execution
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-      fs.writeFile("message.txt", message, (err) => {
-        res.writeHead(302, { Location: "/" });
-        return res.end();
-      });
-    });
-  }
-});
+const server = http.createServer(routes);
 
 server.listen(3000);
