49dfb8cb47dcc7bb3bb3fd3983ae933d3d1951b6
santisdev <104403779+santisdev@users.noreply.github.com>
2023-01-12 10:46:31 -0300

    Mod 5 - app use

--- 5/server/server.js
+++ 5/server/server.js
@@ -4,14 +4,19 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware");
+app.use("/", (req, res, next) => {
+  console.log("This always runs");
   next();
 });
 
-app.use((req, res, next) => {
-  console.log("In the another middleware");
-  res.send("<h1>Hello</h1>");
+app.use("/add-product", (req, res, next) => {
+  console.log("In the add middleware");
+  res.send("<h1>The 'Add product' Page</h1>");
 });
+
+// app.use("/", (req, res, next) => {
+//   console.log("In the another middleware");
+//   res.send("<h1>Hello</h1>");
+// });
 
 app.listen(3000);
