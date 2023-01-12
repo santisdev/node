8383bad485fe26f978776c0270cfb8ba3b4c6630
santisdev <104403779+santisdev@users.noreply.github.com>
2023-01-12 16:37:41 -0300

    Module 5 - Adding routes for admin and shop with router

--- 5/server/server.js
+++ 5/server/server.js
@@ -4,26 +4,13 @@ const express = require("express");
 
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/", (req, res, next) => {
-  console.log("This always runs");
-  next();
-});
+app.use(adminRoutes);
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add product</button></form>"
-  );
-});
-
-app.use("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello</h1>");
-});
+app.use(shopRoutes);
 
 app.listen(3000);
