a390b93c70a4ad1289a2b5ae5f0c420f47d0a961
santisdev <104403779+santisdev@users.noreply.github.com>
2023-01-12 08:14:48 -0300

    Module 5 - Remove create server, express

--- 5/server/server.js
+++ 5/server/server.js
@@ -11,8 +11,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   console.log("In the another middleware");
+  res.send("<h1>Hello</h1>");
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
