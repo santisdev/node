95027bae71f6a7203b3534211bea14b58cb53594
santisdev <104403779+santisdev@users.noreply.github.com>
2023-02-09 17:30:42 -0300

    Module 7 cart

--- 7/server/controllers/shop.js
+++ 7/server/controllers/shop.js
@@ -1,3 +1,4 @@
+const Cart = require("../models/cart");
 const Product = require("../models/product");
 
 exports.getProducts = (req, res, next) => {
@@ -14,6 +15,14 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, (product) => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect("/");
+};
 
 exports.getOrders = (req, res, next) => {};
 
--- README.md
+++ README.md
@@ -3,13 +3,23 @@
 # POST
 
 localhost:3000/admin/add-product
-body: {
-"title": "book1",
-"imageUrl": "https>//image.url",
-"description": "This a new book",
-"price": "400"
-}
+body:
+
+`````json{
+    "title": "book1",
+    "imageUrl": "https>//image.url",
+    "description": "This a new book",
+    "price": "400"
+}```
+
+localhost:3000/cart
+body:
+````json
+{
+"productId":"product Id"
+}```
 
 # GET
 
 localhost:3000/products/prodId
+`````
