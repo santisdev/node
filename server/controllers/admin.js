2b0da1ef7d0e81b0102c3f193ffe126115dbb343
santisdev <104403779+santisdev@users.noreply.github.com>
2023-02-19 14:36:55 -0300

    13 Adding Edit Products

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -5,9 +5,42 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const description = req.body.description;
   const price = req.body.price;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect("/");
+};
+
+exports.getEditProduct = (req, res, netx) => {
+  const editMode = req.query.edit;
+  // if(!editMode) {
+  // return res.send("Error")
+  // }
+  const prodId = req.params.productId;
+
+  Product.findById(prodId, (product) => {
+    if (!product) {
+      res.status(404);
+      return res.send("Error: Product not found");
+    } else {
+      return res.json(product);
+    }
+  });
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.id;
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const description = req.body.description;
+  const price = req.body.price;
+  const updatedProduct = new Product(
+    prodId,
+    title,
+    imageUrl,
+    description,
+    price
+  );
+  updatedProduct.save();
 };
 
 exports.getAddProducts = (req, res, next) => {
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -11,4 +11,8 @@ router.get("/add-product", adminController.getAddProducts);
 // /admin/add-product => POST
 router.post("/add-product", adminController.postAddProduct);
 
+router.get("/edit-product/:productId", adminController.getEditProduct);
+
+router.post("/edit-product", adminController.postEditProduct);
+
 module.exports = router;
